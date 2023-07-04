7c48f925c516107e6c9aefd17908219ecd667bc5
Arya Krishna <dr.aryakris@outlook.com>
2023-07-04 13:33:26 -0700

    added components for button and topnav

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Button(){
+    return(
+        <div>
+        <button>Login</button>
+        <button>Register</button>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Navbar(){
+return(
+    <nav>
+        <img src="../images/logo.png" alt ="logo" />
+        <div className="nav-elements">
+            <ul>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>About</li>
+                <li>Partner</li>
+            </ul>
+        </div>
+        
+      
+    </nav>
+)
+
+}
