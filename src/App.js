a4c39d3a5765a9bb4397f0b07820ed478daeff8e
Arya Krishna <dr.aryakris@outlook.com>
2023-07-31 12:11:51 -0700

    added a new component cards to display the images, a data file to store the data

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Header from "./components/Header";
 import HolidayFinder from "./components/HolidayFinder";
 import Hero from "./components/Hero";
 import Body from "./components/Body";
+import data from "./data"
+import Card from "./components/Card";
 
 
 function App() {
+
+  // const cards = data.map(item =>{
+  //   return (
+  //     <Card
+  //     img = {item.coverImg}/>
+  //   )
+  // })
+
   return (
     <div className="App">
       <Header />
@@ -18,7 +28,11 @@ function App() {
           </div>
           <div className="flex justify-between mx-2 max-w-6xl col-span-2 w-full py-10 ...">
             <Body />
-          </div>
+           </div>
+           <div>
+            <Card/>
+           </div>
+
         </div>
       </div>
     </div >
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+   <div ClassName = "card">
+    <img src= "../images/Raja ampat.png" alt ="" className = "card--image"/>
+    </div>
+      </div>
+  )
+}
+
+export default Card
