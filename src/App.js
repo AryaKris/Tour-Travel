03e09007b562fbdeb2e9fe206de0841e4f4ec3bd
Arya Krishna <dr.aryakris@outlook.com>
2023-08-09 15:22:00 -0700

    Add prettierrc file and update formatting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 import Header from "./components/Header";
 import HolidayFinder from "./components/HolidayFinder";
 import Hero from "./components/Hero";
 import Body from "./components/Body";
-import data from "./data"
+import data from "./data";
 import Card from "./components/Card";
 
-
 function App() {
-
   const images = [
     {
       id: 1,
-      coverImg: 'Raja ampat.png'
+      coverImg: "Raja ampat.png",
     },
     {
       id: 2,
-      coverImg: 'Fanjingshan.png'
+      coverImg: "Fanjingshan.png",
     },
     {
       id: 3,
-      coverImg: 'Vevey.png'
+      coverImg: "Vevey.png",
     },
     {
       id: 4,
-      coverImg: 'Skadar.png'
-    }
-  ]
-
+      coverImg: "Skadar.png",
+    },
+  ];
 
   return (
     <div className="App">
       <Header />
-      <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-right py-6'>
-        <div className="grid grid-cols-2 gap-3 grid-rows-2" >
+      <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-right py-6">
+        <div className="grid grid-cols-2 gap-3 grid-rows-2">
           <Hero />
           <div className="flex justify-between mx-2 max-w-6xl  py-4 col-span-1 ...">
             <HolidayFinder />
@@ -47,8 +44,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
-
 }
 export default App;
