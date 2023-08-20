adab6f544ccd3dc998b28a5c40bcd74242a9e1d9
Arya Krishna <dr.aryakris@outlook.com>
2023-08-19 21:06:52 -0700

    made changes to include the footer component

---
+++ src/components/Stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Stories({ trendingStories }) {
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="col-span-4 ...">
+          <div className="grid grid-cols-2 gap-4">
+            <div ClassName="...">
+              <h1 className="text-4xl text-left font-playfair font-bold">
+                Trending Stories
+              </h1>
+            </div>
+
+            <div ClassName="...">
+              <h1 className="text-right font-inter mx-1 font-semibold text-amber-500 py-6">
+                View all &gt;
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        {trendingStories.map((img) => (
+          <div ClassName="...">
+            <img src={`../images/${img.image}`} alt="" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
